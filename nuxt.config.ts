3b9226efc8799c8354b9f5764a279fafcacdbe7b
Bean <dengbin@bilibili.com>
2025-08-26 14:30:10 +0800

    feat: init project with nuxt4 + tailwind

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  css: [
+    '~/assets/main.css',
+  ],
+  devtools: { enabled: true },
+  eslint: { config: { standalone: false } },
+  modules: ['@nuxt/eslint', '@nuxt/ui'],
+  ssr: false,
+  ui: {
+    colorMode: false,
+  },
+})
